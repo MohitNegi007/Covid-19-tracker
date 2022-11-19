141293daf2f4eb7af90dab29c9879129aaa9f163
Mohit <mohitnegi45801@gmail.com>
2022-11-19 12:03:28 +0530

    all done

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAKPydCIN8OXKkIamHNL3ziJ12kpeHTNtM",
+  authDomain: "covid-19-tracker-e6c81.firebaseapp.com",
+  projectId: "covid-19-tracker-e6c81",
+  storageBucket: "covid-19-tracker-e6c81.appspot.com",
+  messagingSenderId: "76464432404",
+  appId: "1:76464432404:web:f6f485e5a3f0dfa32ace01",
+};
